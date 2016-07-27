3606f0c0d6ad11702856477a804e0b043c4186f5
Jason Govenlock <jason@govenlock.com>
2016-07-27 22:23:09 +1200

    Created MockRepository to assist with unit testing.

--- Web.Tests/Controllers/StatisticsControllerSpecification.cs
+++ Web.Tests/Controllers/StatisticsControllerSpecification.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Web.Mvc;
 using Web.Controllers;
+using Web.Tests;
 
 namespace Web.Tests.Controllers
 {
@@ -11,9 +13,16 @@ namespace Web.Tests.Controllers
         [TestMethod]
         public void Testindex()
         {
-            var controller = new StatisticsController();
+            var mockRepository = new MockRepository();
+
+            var stat = new Statistic() { Id = 4, DateCreated = DateTime.Now, Name = "Test 1", StatisticValue = 24.56M };
+            mockRepository.SaveOrUpdate<Statistic>(stat);
+
+            var controller = new StatisticsController(mockRepository);
 
             var result = controller.Index() as ViewResult;
+
+            //Assert.Equals(stat, result.Model)
         }
     }
 }
---
+++ Web.Tests/MockRepository.cs
@@ -0,0 +1,76 @@
+﻿using Core.Models;
+using Core.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Tests
+{
+    public class MockRepository : IRepository
+    {
+        public Dictionary<Type, IList<Entity>> Items = new Dictionary<Type, IList<Entity>>();
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            if (Items.ContainsKey(typeof(T)))
+            {
+                Items[typeof(T)].Remove(entity);
+            }
+            else
+            {
+                throw new ApplicationException("Unable to delete entity. The type does not exist. Type: " + typeof(T));
+            }
+        }
+
+        public T Get<T>(int id) where T : Entity
+        {
+            if (Items.ContainsKey(typeof(T)))
+            {
+                return Items[typeof(T)].FirstOrDefault(x => x.Id == id) as T;
+            }
+            else
+            {
+                throw new ApplicationException("Unable to return entity. The type does not exist. Type: " + typeof(T));
+            }
+        }
+
+        public IQueryable<T> GetAll<T>() where T : Entity
+        {
+            if (Items.ContainsKey(typeof(T)))
+            {
+                return Items[typeof(T)] as IQueryable<T>;
+            }
+            else
+            {
+                throw new ApplicationException("Unable to return entities. The type does not exist. Type: " + typeof(T));
+            }
+        }
+
+        public void SaveOrUpdate<T>(T entity) where T : Entity
+        {
+            if (Items.ContainsKey(typeof(T)))
+            {
+                var item = Items[typeof(T)].FirstOrDefault(x => x.Id == entity.Id);
+                if (item == null)
+                {
+                    // It's a new item.
+                    Items[typeof(T)].Add(entity);
+                }
+                else
+                {
+                    // Swap item.
+                    var index = Items[typeof(T)].IndexOf(item);
+                    Items[typeof(T)][index] = entity;
+                }
+            }
+            else
+            {
+                // It's a new type.
+                Items[typeof(T)] = new List<Entity>();
+                Items[typeof(T)].Add(entity);
+            }
+        }
+    }
+}
